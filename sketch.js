ad0b0c341aae62db42f274c48b775321e3747b1a
Rhea-Nongmaithem <rhea.nongmaithem2024@bitsdesign.edu.in>
2025-11-12 13:42:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -53,6 +53,7 @@ function draw(){
     fill(random(textcolour));
     textAlign(LEFT);
     text('The first picnic.js is here',windowWidth*(7/16),windowHeight*(2/16));
+    textAlign(LEFT);
     text('and you are invited.',windowWidth*(7/16),windowHeight*(3/16));
   
     fill(random(textcolour));
